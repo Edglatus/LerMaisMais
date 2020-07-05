14c6e953f24da77900ba1ec072636f12e8ce8604
Edgard Wagner <50034857+Edglatus@users.noreply.github.com>
2020-07-05 16:17:43 -0300

    Update index.js

--- src/components/Speech/index.js
+++ src/components/Speech/index.js
@@ -1,8 +1,8 @@
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import {IoMdVolumeHigh, IoMdPause} from 'react-icons/io';
-import { useSpeechSynthesis, speaking, cancel } from 'react-speech-kit';
+import { useSpeechSynthesis } from 'react-speech-kit';
  
 function SpeechComponent({ message }) 
 {
