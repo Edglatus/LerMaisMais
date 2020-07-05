f4ff910e44d8e8780c9e793f86bb33e07ce846f2
matheus1519 <1519matheus@gmail.com>
2020-07-05 15:01:20 -0300

    Try fit speech

--- src/components/Speech/index.js
+++ src/components/Speech/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { AiOutlineSound } from 'react-icons/ai';
 import Speech from 'react-speech';
- 
-export default class SpeechComponent extends Component 
-{    
-    render() 
-    {
+
+import { Container } from './styles';
+
+export default class SpeechComponent extends Component {
+    render() {
         return (
-            <Speech text={this.props.message} textAsButton displayText='Text'/>
+            <Container>
+                <AiOutlineSound color="lightblue" size={36} />
+                <Speech text={this.props.message} ></Speech>
+            </Container>
         );
     }
 }
---
+++ src/components/Speech/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  border:0;
+  background-color:transparent;
+  color: darkblue;
+`;
